57c6461daf183270925fec5dd723c4feb363478f
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-04-24 12:02:07 +0700

    function

---
+++ Module_1/Function/function.js
@@ -0,0 +1,20 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+function printFromOneToTen(){
+    for(let i = 1; i <= 5; i++){
+        console.log(i);
+    }
+}
+printFromOneToTen()
+printFromOneToTen()
+printFromOneToTen()
