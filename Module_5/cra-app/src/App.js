afe1ad7f0534f96ec86ce66d54d47e2760b13452
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-09-12 15:46:52 +0700

    cra vs vite

---
+++ Module_5/cra-app/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// Class Component -> OOP
+// Function Component -> Hooks 
+
+// Component = Element (tag)
+// Hooks = hàm (có sẵn)
+
+// Naming Convention => Pascal Case
+
+class Paragraph extends React.Component{
+  render(){
+    return (
+      // JSX
+      <p>Consequat excepteur tempor ut elit laborum est consequat excepteur sit consequat deserunt. Ex ex anim nisi qui proident sit irure. Nostrud aliquip ex laboris mollit officia. Id irure laboris qui ea enim cupidatat fugiat eu velit sunt ipsum.</p>
+    )
+  }
+}
+
+function Heading(){
+  // logic
+
+  // JSX
+  return (
+    <>
+      <h1>Ut fugiat ex eu incididunt sunt occaecat adipisicing nisi fugiat eu commodo officia cupidatat.</h1>
+      <Paragraph/>
+    </>
+  )
+}
+
+function TextField(){
+  return(
+    <input className='form-control' />
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <TextField/>
+      <Heading/>
+      <Paragraph/>
+    </div>
+  );
+}
+
+export default App;
